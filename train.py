6857f912353406d39675182dd4fa1ef5b6cec4a8
ofoofoo <ofoo@mit.edu>
2024-05-08 00:05:17 -0400

    working towards getting dimensions right

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ import os
 import av
 from model import MoEModel, sample_frame_indices, read_video_pyav
 from transformers import VivitImageProcessor, VivitForVideoClassification
+import numpy as np
 
 # Setup distributed environment
 os.environ['MASTER_ADDR'] = 'localhost'
@@ -36,7 +37,7 @@ dist.init_process_group(backend=backend, init_method='env://', rank=0, world_siz
 
 
 ###### Model - MOE we will be using 
-model = MoEModel(num_experts=8, hidden_size=512).to(device)
+model = MoEModel(num_experts=8, hidden_size=672).to(device)
 
 ####### testing data shape
 
@@ -49,13 +50,18 @@ image_processor = VivitImageProcessor.from_pretrained("google/vivit-b-16x2-kinet
 
 
 # sample 32 frames
-indices = sample_frame_indices(clip_len=32, frame_sample_rate=1, seg_len=container.streams.video[0].frames)
+indices = sample_frame_indices(clip_len=16, frame_sample_rate=2, seg_len=container.streams.video[0].frames)
 video = read_video_pyav(container=container, indices=indices)
-inputs = image_processor(list(video), return_tensors="pt")
+inputs = image_processor(list(video), return_tensors="pt") # down sample to 128*128
 inputs = inputs['pixel_values']
+print(f'Input to ViViT: {inputs.shape}')
 
 print(f'Kinetics 400 Video shape: {video.shape}')
 
+
+inputs=inputs.reshape(inputs.shape[1], inputs.shape[3], -1).to(device) # figure out how to undo this, need to fix this INSIDE of the Moe layer (fairscale) source code?
+print("Reshaped inputs shape: ", inputs.shape)
+#try random size
 
 #what should the input to the model be?
 output= model(inputs)
