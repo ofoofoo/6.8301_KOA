e2b9b40651aff721d86e6db2ced26d676ed1a6c6
ofoofoo <ofoo@mit.edu>
2024-05-13 16:43:07 -0400

    blah

--- train.py
+++ train.py
@@ -265,12 +265,12 @@ def main():
     if args.saverun:
         file_path = "/home/ofoo/MoEViT/results/"
         if args.cifar10:
-            file_path += "cifar10/cifar10_"
+            file_path += "cifar10/depth_scaling/cifar10_"
         if args.cifar100:
             file_path += "cifar100/cifar100_"
         if args.MNIST:
             file_path += "mnist/mnist_"
-        file_path += "512_"
+        file_path += "3_"
         if args.MLP:
             file_path += "MLP"
         if args.KAN: 
